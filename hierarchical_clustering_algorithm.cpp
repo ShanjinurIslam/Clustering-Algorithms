b6d4da972f5f700898e3138eed7424fd79b83968
Shanjinur Islam <spondoncsebuet@gmail.com>
2019-09-29 01:06:05 +0600

    finished with hierarchical_clustering_algorithm

--- hierarchical_clustering_algorithm.cpp
+++ hierarchical_clustering_algorithm.cpp
@@ -3,7 +3,7 @@
 #include <cmath>
 using namespace std;
 
-double calculate_distance(double **le_values,int m1,int m2,int n)
+double calculate_distance(vector<vector<double> > le_values,int m1,int m2,int n)
 {
     double sum = 0;
     for(int i=0;i<n;i++){
@@ -12,21 +12,53 @@ double calculate_distance(double **le_values,int m1,int m2,int n)
     return sqrt(sum);
 }
 
+void printGraph(vector<vector<int> > graph){
+    for(int i=0;i<graph.size();i++){
+        cout << i << ": ";
+        for (int j = 0; j < graph[i].size(); j++)
+        {
+            cout << graph[i][j] << " ";
+        }
+        cout<<endl;
+    }
+}
+
+void printExpressionValues(vector<vector<double> > le_values)
+{
+    for (int i = 0; i < le_values.size(); i++)
+    {
+        cout << i <<": " ;
+        for (int j = 0; j < le_values[i].size(); j++)
+        {
+            cout << le_values[i][j] <<" ";
+        }
+        cout<<endl;
+    }
+}
+
+void printDistanceMatrix(vector<vector<double> > distance_matrix)
+{
+    for (int i = 0; i < distance_matrix.size(); i++)
+    {
+        for (int j = 0; j < distance_matrix[i].size(); j++)
+        {
+            cout << distance_matrix[i][j] << "\t";
+        }
+        cout << endl;
+    }
+}
+
 int main()
 {
-    double **le_values;
     int m,n;
     double value;
     cin>>m>>n;
-    le_values = new double*[m];
-    for(int i=0;i<m;i++){
-        le_values[i] = new double[n];
-    }
+    vector<vector<double> > le_values(m);
     
     for(int i=0;i<m;i++){
         for(int j=0;j<n;j++){
             cin>>value;
-            le_values[i][j] = value;
+            le_values[i].push_back(value);
         }
     }
     
@@ -48,14 +80,97 @@ int main()
             }
         }
     }
-    for (int i = 0; i < m; i++)
-    {
-        for (int j = 0; j < m; j++)
+    
+
+    vector<vector<int> > graph(m);
+    int number_of_clusters = m;
+    while(number_of_clusters>2){
+
+        double min = 1e10;
+        int c1,c2 ;
+       
+        for (int i = 0; i < distance_matrix.size(); i++)
         {
-            cout << distance_matrix[i][j] << " ";
+            for (int j = 0; j < distance_matrix.size(); j++)
+            {
+                if (i != j && distance_matrix[i][j] != -1)
+                {
+                    if(distance_matrix[i][j]<min){
+                        min = distance_matrix[i][j];
+                        c1 = i ;
+                        c2 = j ;
+                    }
+                }
+            }
         }
-        cout << endl;
+        cout<<c1<<" "<<c2<<endl;
+        graph[c1].push_back(c2);
+        graph[c2].push_back(c1);
+        vector<int> c ;
+        c.push_back(c1);
+        c.push_back(c2);
+        graph.push_back(c);
+        graph[c1].push_back(graph.size()-1);
+        graph[c2].push_back(graph.size()-1);
+        //printGraph(graph);
+        vector<double> gene_values ;
+        for(int i=0;i<n;i++){
+            gene_values.push_back(le_values[c1][i]);
+            gene_values[i] += le_values[c2][i];
+        }
+        for (int i = 0; i < n; i++)
+        {
+            gene_values[i] /= 2;
+        }
+        le_values.push_back(gene_values);
+        //column calculation
+        for (int i = 0; i < le_values.size()-1; i++)
+        {
+            distance_matrix[i].push_back(calculate_distance(le_values, i, le_values.size()-1,n));
+        }
+
+        //row calculation
+        vector<double> tmp;
+        
+        for (int i = 0; i < le_values.size() ; i++)
+        {
+            tmp.push_back(calculate_distance(le_values, le_values.size()-1, i, n));
+        }
+        distance_matrix.push_back(tmp);
+        int last = distance_matrix.size()-1 ;
+        for (int i = 0; i < le_values.size(); i++)
+        {
+            if(distance_matrix[i][0]==-1){
+                distance_matrix[last][i] = -1;
+                distance_matrix[i][last] = -1;
+            }
+        }
+        //printDistanceMatrix(distance_matrix);
+        cout<<endl;
+        for (int i = 0; i < distance_matrix[c1].size();i++){
+            distance_matrix[c1][i] = -1 ;
+        }
+        for (int i = 0; i < distance_matrix[c2].size(); i++)
+        {
+            distance_matrix[c2][i] = -1;
+        }
+        for (int i = 0; i < distance_matrix.size(); i++)
+        {
+            distance_matrix[i][c1] = -1;
+        }
+        for (int i = 0; i < distance_matrix.size(); i++)
+        {
+            distance_matrix[i][c2] = -1;
+        }
+        printDistanceMatrix(distance_matrix);
+        
+        cout<<endl;
+        number_of_clusters--;
     }
+
+
+    cout<<"Final out graph:: "<<endl;
+    printGraph(graph);
 
     return 0;
 }
